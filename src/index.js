b26de8610b703535579a98c6348d86acee41f4c1
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-08 12:26:25 +0300

    подключение BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './components/App';
-// import Header from './components/Header';
-import Main from './components/Main';
-// import Footer from './components/Footer';
-// import PopupWithForm from './components/PopupWithForm';
-// import ImagePopup from './components/ImagePopup';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>),
     document.getElementById('root')
 );
