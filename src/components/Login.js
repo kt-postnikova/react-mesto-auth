3a5ffb36347e27754b6c2d90c3a88e28499fe6a6
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-08 15:18:47 +0300

    добавлены верстка и стили для экрана входа

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Login() {
     return (
-        <div></div>
+        <div className="register">
+            <h2 className="register__title">Вход</h2>
+            <form className="register__form">
+                <input className="register__input" placeholder="Email" type="email" />
+                <input className="register__input" placeholder="Пароль" type="password" />
+                <button className="register__button">Войти</button>
+            </form>
+        </div>
     );
 }
 
