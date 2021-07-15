06602e36ab685814d89caa413ede553068118a22
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-15 13:00:53 +0300

    Image и info меняются в зависимости от ответа

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -5,8 +5,8 @@ function InfoTooltip(props) {
         <div className={`popup popup_type_${props.name} ${props.isOpen ? ('popup_opened') : ''}`}>
             <div className="popup__container">
                 <button className="popup__close" onClick={props.onClose} type="button" aria-label="Закрыть"></button>
-                <div className="tooltip__image"></div>
-                <p className="tooltip__info">Вы успешно зарегистрировались!</p>
+                <img className="tooltip__image" src={props.image} />
+                <p className="tooltip__info">{props.info}</p>
             </div>
         </div>
     );
