02a07b4fdb74a747ea87b2e4bfabd7315ea22b3b
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-15 12:59:25 +0300

    компонент Login создан

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
-function Login() {
+function Login(props) {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    function handleChangeEmail(e) {
+        setEmail(e.target.value)
+    }
+
+    function handleChangePassword(e) {
+        setPassword(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        props.onAuth(password, email)
+    }
+
     return (
         <div className="register">
             <h2 className="register__title">Вход</h2>
-            <form className="register__form">
-                <input className="register__input" placeholder="Email" type="email" />
-                <input className="register__input" placeholder="Пароль" type="password" />
+            <form className="register__form" onSubmit={handleSubmit}>
+                <input
+                    className="register__input"
+                    value={email}
+                    onChange={handleChangeEmail}
+                    placeholder="Email"
+                    type="email" />
+                <input
+                    className="register__input"
+                    value={password}
+                    onChange={handleChangePassword}
+                    placeholder="Пароль"
+                    type="password" />
                 <button className="register__button">Войти</button>
             </form>
         </div>
