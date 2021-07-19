bf746984a22a1f9611b55715066e82c69d4cfa2a
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-19 11:55:43 +0300

    добавлен alt для картинки с ошибкой

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -5,7 +5,7 @@ function InfoTooltip({ name, isOpen, onClose, image, info }) {
         <div className={`popup popup_type_${name} ${isOpen ? ('popup_opened') : ''}`}>
             <div className="popup__container">
                 <button className="popup__close" onClick={onClose} type="button" aria-label="Закрыть"></button>
-                <img className="tooltip__image" src={image} />
+                <img className="tooltip__image" src={image} alt={info} />
                 <p className="tooltip__info">{info}</p>
             </div>
         </div>
